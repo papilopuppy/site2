157d03b982da4b10c2f445ab4a77ddf8ae2963f0
papilopuppy <ccojocaru@lightology.com>
2014-08-29 15:01:34 -0500

    My secondest commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+ <link href="css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+ <header>
+  <nav>
+   <ul>
+    <li><a href="/">Menu 1</a></li>
+    <li><a href="/">Menu 2</a></li>
+    <li><a href="/">Menu 3</a></li>
+   </ul>
+  </nav>
+ </header>
+
+ <section class="wrapper">
+  <div class="left-nav">
+   <a href="/" class="butt">Button</a>
+  </div>
+  <div class="right-nav">
+   <a href="/" class="butt2">Button</a>
+  </div>
+ </section>
+
+ <div class="popup"><span></span></div>
+
+ <table>
+  <thead>
+   <th>
+    <td>Head</td>
+   </th>
+  </thead>
+  <tbody>
+   <tr><td>one</td></tr>
+   <tr><td>two</td></tr>
+   <tr><td>three</td></tr>
+   <tr><td>four</td></tr>
+   <tr><td>five</td></tr>
+  </tbody>
+ </table>
+
+</body>
+
+</html>
